a09469d52360068eba3f92cbc1dc25dfe110cf10
leolmzhang <leolmzhang@tencent.com>
2022-11-07 14:17:59 +0800

    change  cpplint length limit 80 to 120 and add .clang-format

--- callback-function/main.cpp
+++ callback-function/main.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 
-#include <functional> // fucntion/bind
+#include <functional>  // fucntion/bind
 
 class ClassCalled {
-public:
-  void FunCalled1() {
-    std::cout << "ClassCalled.FunCalled1() called.." << std::endl;
-  }
+ public:
+  void FunCalled1() { std::cout << "ClassCalled.FunCalled1() called.." << std::endl; }
 
-  void FunCalled2() {
-    std::cout << "ClassCalled.FunCalled2() called.." << std::endl;
-  }
+  void FunCalled2() { std::cout << "ClassCalled.FunCalled2() called.." << std::endl; }
   bool FunCalled5(int s) {
     std::cout << "ClassCalled.FunCalled2() called.. " << s << std::endl;
     return s > 0;
   }
 
-  static void FunCalled3() {
-    std::cout << "ClassCalled.FunCalled3() called.." << std::endl;
-  }
+  static void FunCalled3() { std::cout << "ClassCalled.FunCalled3() called.." << std::endl; }
 };
 
 class ClassCaller {
-  using CallbackFun = std::function<void()>;
+  using CallbackFun     = std::function<void()>;
   using CallbackFunBool = std::function<bool(int s)>;
 
-public:
+ public:
   void FunCaller(CallbackFun callback) {
     std::cout << "ClassCaller.FunCaller() called.." << std::endl;
     callback();
@@ -36,19 +30,20 @@ public:
   }
 };
 
-void GlobalFun() { std::cout << "GlobalFun() called.." << std::endl; }
+void GlobalFun() {
+  std::cout << "GlobalFun() called.." << std::endl;
+}
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   printf("\n-------------\n");
   ClassCalled called;
   called.FunCalled1();
-  int var = int(atoi(argv[1]));
+  int         var = atoi(argv[1]);
   ClassCaller caller;
   caller.FunCaller(GlobalFun);
   caller.FunCaller(ClassCalled::FunCalled3);
   caller.FunCaller(std::bind(&ClassCalled::FunCalled2, &called));
-  auto temp = caller.FunCallerBool(
-      std::bind(&ClassCalled::FunCalled5, &called, std::placeholders::_1), var);
+  auto temp = caller.FunCallerBool(std::bind(&ClassCalled::FunCalled5, &called, std::placeholders::_1), var);
   std::cout << "return : " << temp << std::endl;
   printf("\n-------------\n");
 }

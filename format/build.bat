897013f85038e904494d62beb1f7ee64db4626fa
leolmzhang <leolmzhang@tencent.com>
2023-12-18 19:38:14 +0800

    add format

---
+++ format/build.bat
@@ -0,0 +1,2 @@
+cmake -B build -DBUILD_SHARED_LIBS=true
+cmake --build ./build --config Release
--- timer/build.bat
+++ timer/build.bat
@@ -1,2 +1,2 @@
 cmake -B build -DBUILD_SHARED_LIBS=true -DENABLE_TADSIM_MODULE=ON
-cmake --build ./build --config Release
+cmake --build ./build --config Debug
